cf6f630807fd3f9478fa77900755ec769b9bdef2
Mudasser Patel <patelmudasser126@gmail.com>
2024-01-28 13:08:24 +0800

    Here's my JavaScript Intro Assessment

---
+++ javascript_file.js
@@ -0,0 +1,9 @@
+/*Setting the variables*/
+var firstName = "Mudasser";
+var lastName = "Patel";
+var age = "25";
+
+/*Creating alerts*/
+alert("My Name is " + firstName +" "+ lastName);
+alert("I am "+age+" years old");
+console.log("I love programming, that is why I'm having fun in this class.");
